f8b0db315cd15f8e438317965d1324d1b38a819a
Julien <julien.guinot@artefact.com>
2021-08-22 11:09:32 +0200

    start building preprocessing pipeline

---
+++ src/Training_pipeline.py
@@ -0,0 +1,19 @@
+import spleeter
+import pandas as pd
+import yaml
+
+with open("../config/config.yaml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
+
+
+def main_pipeline():
+    pass
+
+def split_song(path):
+    pass
+
+def split_folder(path):
+    pass
+
+def build_dataset(path):
+    pass
